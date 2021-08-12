9ad12b17c985447eb8c4fbced14d9d99bb826d04
dynamite14 <78571139+dynamite14@users.noreply.github.com>
2021-08-11 21:27:07 -0400

    tweak code

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 # TOKEN = "MANUAL OVERRIDE"
 
-prefix = 'c!'
+prefix = 'd!'
 gameStatus = ""
 
 # activity = Game(name=gameStatus)
@@ -33,13 +33,6 @@ testServers = [738488607261851748,
 bot_owners  = [642527833410895882]
 
 bot_masters = [642527833410895882]
-
-with open("botmasters.txt", "r") as f:
-    bot_masters = []
-    bot_masters_str = f.readlines()
-
-for userid in bot_masters_str:
-    bot_masters.append(int(userid))
 
 @bot.command(name='senkobread')
 async def senko(ctx):
@@ -82,45 +75,5 @@ async def changeactivity(ctx, *args):
             
     else:
         await ctx.send(":x: Access denied. You must be a **Bot Master** to use this command.")
-
-@bot.command(name='botmaster')
-async def managemasters(ctx, *args):
-    if ctx.author.id in bot_owners:
-        args = list(args)
-        if args[0] == 'list':
-            master_list = ""
-            for i in bot_masters:
-                master_list = master_list + "\n" + str(i)
-            
-            await ctx.send(embed=Embed(title="Bot Masters", description=master_list))
-
-        elif args[0] in ['add','remove']:
-            try:
-                this_id = str(args[1])
-            except (ValueError, TypeError):
-                await ctx.send(embed=Embed(title="Value Error",description=f"User ID must be an integer.", color=0xff0000))
-            except KeyError:
-                await ctx.send(embed=Embed(title="Error",description=f"Not enough arguments\n\nProper command format: `{prefix}botmaster <add|remove> <id>`", color=0xff0000))
-            else:
-                if args[0] == 'add':
-                    bot_masters.append(this_id)
-                    with open("botmasters.txt", 'w') as f:
-                        f.writelines(bot_masters)
-                elif args[0] == 'remove':
-                    try:
-                        bot_masters.remove(this_id)
-                    except ValueError:
-                        await ctx.send(embed=Embed(title="ID Error",description=f"User ID is not already a botmaster!", color=0xff0000))
-                    else:
-                        with open("botmasters.txt", 'w') as f:
-                            f.writelines(bot_masters)
-                else:
-                    await ctx.send(embed=Embed(title="Error",description=f"Improper arguments\n\nProper command format: `{prefix}botmaster <list|add|remove> [id]`", color=0xff0000))
-        
-        else:
-            await ctx.send(embed=Embed(title="Error",description=f"Improper arguments\n\nProper command format: `{prefix}botmaster <list|add|remove> [id]`", color=0xff0000))
-
-    else:
-        await ctx.send(":x: Access denied. You must be a **Bot Owner** to use this command.")
 
 bot.run(TOKEN)
